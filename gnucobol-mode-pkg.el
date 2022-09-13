917b58b67d6a7082e48abd0c92318a83837d10c1
wmealing <wmealing@gmail.com>
2022-09-13 23:25:00 +1000

    Add basic package.

---
+++ gnucobol-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from gnucobol-mode.el  -*- no-byte-compile: t -*-
+(define-package "gnucobol-mode" "1.0.0" "Mode for editing GnuCOBOL code" '((cl-lib "0.5")) :url "http://elpa.gnu.org/packages/gnucobol-mode.html" :keywords '("languages") :authors '(("Wade Mealing" . "wmealing@gmail.com")) :maintainer '("Wade Mealing" . "wmealing@gmail.com"))
